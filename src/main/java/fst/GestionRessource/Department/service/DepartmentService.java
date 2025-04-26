51a935fd6711dd1c5d94f43c12c7c28dc0c34e1e
Ouail Kerrak <164376403+OuailKe@users.noreply.github.com>
2025-04-26 14:32:25 +0100

    Update DepartmentService.java
    name fix

--- src/main/java/fst/GestionRessource/Department/service/DepartmentService.java
+++ src/main/java/fst/GestionRessource/Department/service/DepartmentService.java
@@ -10,7 +10,7 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class DepartmentService {
+public class DepartmentServiceImpl {
   private final DepartmentRepository repository;
 
   public ResponseEntity<?> getAllDepartments() {
