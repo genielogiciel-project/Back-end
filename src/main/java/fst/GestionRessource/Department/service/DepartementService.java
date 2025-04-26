807c23f24e781597f1bfb05cf4a33e854d91db6d
Ouail Kerrak <164376403+OuailKe@users.noreply.github.com>
2025-04-26 14:32:49 +0100

    Update DepartementService.java
    .

--- src/main/java/fst/GestionRessource/Department/service/DepartementService.java
+++ src/main/java/fst/GestionRessource/Department/service/DepartementService.java
@@ -1,4 +1,13 @@
 package fst.GestionRessource.Department.service;
 
 public interface DepartementService {
+   public ResponseEntity<?> getAllDepartments();
+
+   public ResponseEntity<?> getDepartmentById(String id);
+
+   public ResponseEntity<?> addDepartment(Department department);
+
+   public ResponseEntity<?> updateDepartment(String id, Department department);
+
+   public ResponseEntity<?> deleteDepartment(String id);
 }
