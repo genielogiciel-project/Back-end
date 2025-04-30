2962caf20f8c51b092199cd738aef305611f5470
Mohammed Amine El Abiad <amineprogrammation@gmail.com>
2025-04-30 03:29:41 +0000

    ..

--- src/main/java/fst/GestionRessource/Configuration/OtherUsers.java
+++ src/main/java/fst/GestionRessource/Configuration/OtherUsers.java
@@ -28,7 +28,7 @@ public class OtherUsers implements ApplicationListener<ContextRefreshedEvent> {
   public void onApplicationEvent(ContextRefreshedEvent event) {
     if (userRepository.count() >=6) return;
     var users = List.of(
-      new User("U-00000000000000000000000000000002", "head", "chef de departement", passwordEncoder.encode("0"), Collections.singletonList(Role.DEPARTMENT_HEAD), null, new Department("DEPT-4c14dd4c-0e35-4bc1-9a3f-27992019c468", null, null, null, null, null)),
+      new User("U-00000000000000000000000000000002", "head", "chef de departement", passwordEncoder.encode("0"), Collections.singletonList(Role.DEPARTMENT_HEAD), null, null),
       new User("U-00000000000000000000000000000003", "teacher", "enseignant", passwordEncoder.encode("0"), Collections.singletonList(Role.TEACHER), null, null),
       new User("U-00000000000000000000000000000004", "supplier", "fournisseur", passwordEncoder.encode("0"), Collections.singletonList(Role.SUPPLIER), null, null),
       new User("U-00000000000000000000000000000005", "resmanager", "responsable des ressources", passwordEncoder.encode("0"), Collections.singletonList(Role.RESOURCE_MANAGER), null, null),
